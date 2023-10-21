3cfda3f0438f0d522be056b8ce33e336ed5c051a
jeffassis <cmdtjeff@gmail.com>
2023-10-21 19:19:49 -0300

    first commit

---
+++ src/main/java/br/com/jeff/restescola/RestEscolaApplication.java
@@ -0,0 +1,13 @@
+package br.com.jeff.restescola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestEscolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestEscolaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jeff/restescola/RestEscolaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jeff.restescola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestEscolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
